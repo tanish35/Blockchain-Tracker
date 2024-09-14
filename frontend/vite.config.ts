4ee6a765dc5de9f6cbe1c552d0fd6439f7db11a2
tanish35 <tanishmajumdar2912@gmail.com>
2024-09-14 12:17:59 +0530

    Fixed local network

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Fix: Move the `server` object inside the main config object
 export default defineConfig({
   plugins: [react()],
   server: {
