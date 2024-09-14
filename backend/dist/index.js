46a270a5b252a27a7a948e1e8b894ada9f77dedd
tanish35 <tanishmajumdar2912@gmail.com>
2024-09-14 14:17:05 +0530

    Update CORS

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -10,6 +10,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const walletRoutes_1 = __importDefault(require("./routes/walletRoutes"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const corsOptions = {
     origin: [
         "http://localhost:3001",
@@ -20,6 +21,9 @@ const corsOptions = {
 app.use((0, cors_1.default)(corsOptions));
 app.use((0, cookie_parser_1.default)());
 app.use("/api/wallet", walletRoutes_1.default);
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on port ${process.env.PORT}`);
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,9 @@ const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const corsOptions = {
   origin: [
     "http://localhost:3001",
+    "http://0.0.0.0:5173",
     "http://localhost:5173",
+    "http://192.168.29.39:5173",
   ],
   credentials: true,
 };
